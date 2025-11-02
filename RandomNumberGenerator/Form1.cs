c640b00148ba4a5085f8f8f10fadca163bfa8e24
Phone Htet Paing <phonehtetpaing990@gmail.com>
2025-11-03 04:36:37 +0530

    Event Input Error Handling Done

--- RandomNumberGenerator/Form1.cs
+++ RandomNumberGenerator/Form1.cs
@@ -21,19 +21,43 @@ namespace RandomNumberGenerator
 
         private void algorithm_choice_SelectedIndexChanged(object sender, EventArgs e)
         {
-            algorithm = algorithm_choice.SelectedItem?.ToString();
+            try
+            {
+                algorithm = algorithm_choice.SelectedItem?.ToString();
+            }
+            catch
+            {
+                algorithm = null;
+            }
             Algorithms.seed = 0;
         }
 
         private void generate_button_Click(object sender, EventArgs e)
         {
             number_show.Text = "";
-            length = Convert.ToUInt32(length_of_number.Text);
-            Algorithms.seed = Convert.ToInt32(seed_input.Text);
+            try
+            {
+                length = Convert.ToUInt32(length_of_number.Text);
+                Algorithms.seed = Convert.ToInt32(seed_input.Text);
+            } 
+            catch
+            {
+                MessageBox.Show("Please Fill the Length and the Seed!");
+            }
+            if (algorithm == null)
+            {
+                MessageBox.Show("Please Select the Algorithm!");
+                return;
+            }
+            if (Algorithms.seed == 0)
+            {
+                MessageBox.Show("Seed cannot be Zero!");
+                return;
+            }
             for (int i = 0; i < length; i++)
             {
                 number_show.Text += Algorithms.wrapper_func(algorithms_pair[algorithm!]);
-                Debug.WriteLine(Algorithms.state);
+                // Debug.WriteLine(Algorithms.state);
             }
         }
     }
